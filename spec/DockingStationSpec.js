33e0835a5bb4f8f7108e147e68dae1f6130c81e3
CKKH <chriskkhassan@gmail.com>
2019-01-17 10:52:04 +0000

    Refactored tests, dockingStationOne created with a before each function

--- spec/DockingStationSpec.js
+++ spec/DockingStationSpec.js
@@ -1,12 +1,14 @@
 describe("DockingStation", () => {
 
+  beforeEach( () => {
+    dockingStationOne = new DockingStation();
+  });
+
   it("instance of DockingStation can be stored as a variable", () => {
-    var dockingStationOne = new DockingStation();
     expect(dockingStationOne instanceof DockingStation).toBe(true);
   });
 
   it("reponds to the .dockedBikes method", () => {
-    var dockingStationOne = new DockingStation();
     expect(dockingStationOne.dockedBikes()).toBeDefined();
   });
 
