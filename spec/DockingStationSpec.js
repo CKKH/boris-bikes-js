72113493fb1fcdcc904f07d89fb88fe0d6dfcc64
CKKH <chriskkhassan@gmail.com>
2019-01-17 15:04:51 +0000

    Refactored: DockingStation tested in isolation by using bike Spy

--- spec/DockingStationSpec.js
+++ spec/DockingStationSpec.js
@@ -2,6 +2,7 @@ describe("DockingStation", () => {
 
   beforeEach( () => {
     dockingStationOne = new DockingStation();
+    bike = jasmine.createSpy('bike')
   });
 
   it("instance of DockingStation can be stored as a variable", () => {
@@ -13,7 +14,6 @@ describe("DockingStation", () => {
   });
 
   it("stores a bike in it's array after .dock(bike) called", () => {
-    var bike = new Bike();
     dockingStationOne.dock(bike);
     expect(dockingStationOne.dockedBikes()).toContain(bike);
   });
