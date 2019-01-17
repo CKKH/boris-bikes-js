4815356037249eb476ee7012a74c4c02208396ce
CKKH <chriskkhassan@gmail.com>
2019-01-17 11:55:14 +0000

    Refactored tests - specific test checking .dockedBikes being definded redundant

--- spec/DockingStationSpec.js
+++ spec/DockingStationSpec.js
@@ -8,10 +8,6 @@ describe("DockingStation", () => {
     expect(dockingStationOne instanceof DockingStation).toBe(true);
   });
 
-  it("reponds to the .dockedBikes method", () => {
-    expect(dockingStationOne.dockedBikes()).toBeDefined();
-  });
-
   it("returns an array of docked bikes when .dockedBikes called", () => {
     expect(dockingStationOne.dockedBikes()).toEqual([]);
   });
