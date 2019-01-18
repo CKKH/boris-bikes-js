c37d314a1d2390f6014da77c0ae63481d177a7bd
CKKH <chriskkhassan@gmail.com>
2019-01-18 15:52:56 +0000

    Test refactor: grouped dockingStation tests in groups by method

--- spec/DockingStationSpec.js
+++ spec/DockingStationSpec.js
@@ -15,24 +15,30 @@ describe("DockingStation", () => {
     expect(dockingStationOne instanceof DockingStation).toBe(true);
   });
 
-  it("returns an array of docked bikes when .dockedBikes called", () => {
-    expect(dockingStationOne.dockedBikes()).toEqual([]);
+  describe("#dockedBikes", () => {
+    it("returns an array of docked bikes when .dockedBikes called", () => {
+      expect(dockingStationOne.dockedBikes()).toEqual([]);
+    });
   });
 
-  it("stores a bike in it's array after .dock(bike) called", () => {
-    dockingStationOne.dock(bike);
-    expect(dockingStationOne.dockedBikes()).toContain(bike);
+  describe("#dock(bike)", () => {
+    it("stores a bike in it's array after .dock(bike) called", () => {
+      dockingStationOne.dock(bike);
+      expect(dockingStationOne.dockedBikes()).toContain(bike);
+    });
   });
 
-  it("releases a docked bike from it's array after .release(bike) called", () => {
-    dockingStationOne.dock(bike);
-    dockingStationOne.release(bike);
-    expect(dockingStationOne.dockedBikes()).not.toContain(bike);
-  });
+  describe("#release(bike)", () => {
+    it("releases a docked bike from it's array after .release(bike) called", () => {
+      dockingStationOne.dock(bike);
+      dockingStationOne.release(bike);
+      expect(dockingStationOne.dockedBikes()).not.toContain(bike);
+    });
 
-  it("raises error if .release(bike) called when dockingStation empty", () => {
-    var error = "Cannot release bike: dockingStation empty"
-    expect(function() { dockingStationOne.release(bike) }).toThrow(new Error(error));
+    it("raises error if .release(bike) called when dockingStation empty", () => {
+      var error = "Cannot release bike: dockingStation empty"
+      expect(function() { dockingStationOne.release(bike) }).toThrow(new Error(error));
+    });
   });
 
 });
